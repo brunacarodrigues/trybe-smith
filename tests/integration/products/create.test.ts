7cf51eb9ae4096535ac02695914cb96138e6ef0a
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-08-28 15:15:33 -0300

    tests

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,26 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import productMock from '../../mocks/Product.mock';
+import productModel from '../../../src/database/models/product.model';
+import listProductsMock from '../../mocks/listProducts.mock';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
+
+  it('o produto foi cadastrado com sucesso', async function () {
+    // const result = productModel.build(productMock.mockOneProduct);
+    sinon.stub(productModel, 'create').resolves(productMock.mockOneProduct as any);
+
+    const response = await chai
+      .request(app)
+      .post('/products')
+      .send(productMock.mockOneProduct);
+
+    expect(response).to.have.status(201);
+  })
 
 });
--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,21 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import productModel from '../../../src/database/models/product.model';
+import listProductsMock from '../../mocks/listProducts.mock';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('GET /products', function () { 
   beforeEach(function () { sinon.restore(); });
   
+  it('retorna todos os produtos cadastrados', async function () {
+    // const allProducts = productModel.bulkBuild(listProductsMock.mockListProducts)
+    sinon.stub(productModel, 'findAll').resolves(listProductsMock.mockListProducts as any)
+
+    const response = await chai.request(app).get('/products').send()
+    expect(response.status).to.be.equal(200)
+  })
+
 });
